3a995cd9b0b4a24aabd4b02952291d648280114e
Daiwei Chen <kinbd8@gmail.com>
2018-10-25 09:18:46 -0500

    Changed leds code to run async

--- leds/index.js
+++ leds/index.js
@@ -2,37 +2,53 @@
 
 // Import the interface to Tessel hardware
 const tessel = require('tessel');
-
+var rand = 0;
 var greenPin = tessel.port.A.pin[0];
 var redPin = tessel.port.A.pin[1];
-greenPin.write(1, (error, buffer) => {
-	if(error){
-		throw error;
-	}
-
-	console.log(buffer.toString());
-});
+var yellowPin = tessel.port.A.pin[2];
 
 // Blink!
+rand = Math.floor(Math.random() * 1001 + 100);
 setInterval(() => {
   tessel.led[2].toggle();
-  tessel.led[3].toggle();
+}, rand);
 
+rand = Math.floor(Math.random() * 1001 + 100);
+setInterval(()=> {
+  tessel.led[3].toggle();
+}, rand);
+
+rand = Math.floor(Math.random() * 1001 + 100);
+setInterval(()=> {
   greenPin.toggle((error, buffer) => {
     if (error) {
       throw error;
     }
 
-    console.log(buffer.toString()); // Log the value written to the pin
+    console.log(buffer.toString('hex')); // Log the value written to the pin
   });
+}, rand);
 
+rand = Math.floor(Math.random() * 1001 + 100);
+setInterval(()=> {
   redPin.toggle((error, buffer) => {
     if (error) {
       throw error;
     }
 
-    console.log(buffer.toString()); // Log the value written to the pin
+    console.log(buffer.toString('hex')); // Log the value written to the pin
   });
-}, 100);
+}, rand);
+
+rand = Math.floor(Math.random() * 1001 + 100);
+setInterval(()=> {
+  yellowPin.toggle((error, buffer) => {
+    if (error) {
+      throw error;
+    }
+
+    console.log(buffer.toString('hex')); // Log the value written to the pin
+  });
+}, rand);
 
 console.log("I'm blinking! (Press CTRL + C to stop)");
